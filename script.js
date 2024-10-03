87e48496120a983447d05fe66e096d0e5b6d1f94
wallacyAmauri <wallacya087@gmail.com>
2024-10-03 19:07:37 -0300

    1.5

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Exibir o modal após 10 segundos
+    setTimeout(() => {
+        const modal = document.getElementById("modal");
+        modal.style.display = "block";
+    }, 1000);
+
+    // Fechar o modal quando o usuário clicar no botão "fechar"
+    document.getElementById("close").onclick = function() {
+        const modal = document.getElementById("modal");
+        modal.style.display = "none";
+    }
+
+    // Ações para os botões Aceitar e Recusar
+    document.getElementById("accept").onclick = function() {
+        alert("Vamos ao cinema!, agora escolhe o filme que você quer? selecionei todos que você gosta, agora só você escolher o que quer ver");
+        document.getElementById("modal").style.display = "none";
+    }
+
+    document.getElementById("decline").onclick = function() {
+        alert("Sem problemas! Talvez em outra ocasião. :(");
+        document.getElementById("modal").style.display = "none";
+    }
+});
 function toggleVideo(){
     const trailer = document.querySelector('.trailer');
     const video = document.querySelector('video');
